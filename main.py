2c9cf2f15760322b732a282adf6fa8e02c5df5a6
Nikhil <nikhil342015@gmail.com>
2023-02-14 01:27:34 -0800

    Graph Visualization Updated

--- main.py
+++ main.py
@@ -12,118 +12,21 @@ class Packet(object):
     def __init__(self, node = None): 
         self.node = node
 
-
-
-
-
-def dijkstra(graph, firstNode):
-    unvisitedNodes = list(graph.returnNodes())   
-    shortestPathValues = {}
-
-    #Set distance to all nodes as infinity
-    for node in unvisitedNodes:
-        shortestPathValues[node] = [sys.maxsize, ""]
-
-    #Set distance to starting node as 0
-    shortestPathValues[firstNode][0] = 0
-    previousNodes = {}
-
-    #Find the closest node and update the shortest path to it
-    while unvisitedNodes:
-        closestNode = unvisitedNodes[0]
-        for node in unvisitedNodes:
-            if shortestPathValues[node][0] < shortestPathValues[closestNode][0]:
-                closestNode = node
-
-        neighbors = graph.returnEdges(closestNode)
-
-        for neighbor in neighbors:
-            newPathLength = shortestPathValues[closestNode][0] + graph.returnValue(closestNode, neighbor)[0]
-            if newPathLength < shortestPathValues[neighbor][0]:
-                shortestPathValues[neighbor][0] = newPathLength
-                shortestPathValues[neighbor][1] = graph.returnValue(closestNode, neighbor)[1]
-                previousNodes[neighbor] = closestNode
-        unvisitedNodes.remove(closestNode)
-    
-    return previousNodes, shortestPathValues
-
-def dijkstraMod(graph, firstNode):
-    unvisitedNodes = list(graph.returnNodes())   
-    shortestPathValues = {}
-
-    #Set distance to all nodes as infinity
-    for node in unvisitedNodes:
-        shortestPathValues[node] = [sys.maxsize, ""]
-
-    #Set distance to starting node as 0
-    shortestPathValues[firstNode][0] = 0
-    previousNodes = {}
-
-    #Find the closest node and update the shortest path to it
-    while unvisitedNodes:
-        closestNode = unvisitedNodes[0]
-        for node in unvisitedNodes:
-            if shortestPathValues[node][0] < shortestPathValues[closestNode][0]:
-                closestNode = node
-        neighbors = graph.returnEdges(closestNode)
-        wired = False
-        for neighbor in neighbors:
-            newPathLength = shortestPathValues[closestNode][0] + graph.returnValue(closestNode, neighbor)[0]
-            if newPathLength < shortestPathValues[neighbor][0] and graph.returnValue(closestNode, neighbor)[1] == "wired":
-                wired = True
-                shortestPathValues[neighbor][0] = newPathLength
-                shortestPathValues[neighbor][1] = graph.returnValue(closestNode, neighbor)[1]
-                previousNodes[neighbor] = closestNode
-        if wired == False:
-            for neighbor in neighbors:
-                newPathLength = shortestPathValues[closestNode][0] + graph.returnValue(closestNode, neighbor)[0]
-            if newPathLength < shortestPathValues[neighbor][0]:
-                wired = True
-                shortestPathValues[neighbor][0] = newPathLength
-                shortestPathValues[neighbor][1] = graph.returnValue(closestNode, neighbor)[1]
-                previousNodes[neighbor] = closestNode
-                
-        unvisitedNodes.remove(closestNode)
-    
-    return previousNodes, shortestPathValues
-   
-def display(previousNodes, graphDictionary, firstNode, lastNode):
-    path = []
-    node = lastNode
-    
-    while node != firstNode:
-        path.append(node)
-        node = previousNodes[node]
-    
-    path.append(firstNode)
-    
-    for i in range(0, len(path)-1):
-        if graphDictionary[path[i]][path[i+1]][1] == "wireless":
-            randomNum = random.randint(0,3)
-            if randomNum == 0:
-                raise Exception("Wireless node has failed")
-        else:
-            randomNum = random.randint(0, 19)
-            if randomNum == 0:
-                raise Exception("Wired node has failed")
-        print(path[i] + "   -" + str(graphDictionary[path[i]][path[i+1]]) + "->   ", end ="")
-    print(firstNode)
-
-def generateVisual(graphDictionary, nodes):
+def generateVisual(branches, routers):
     # Simulates pyvis graph visualizer
     net = Network()
 
     # Add nodes to graph
-    for node in nodes:
-        net.add_node(node, label=str(node))
+    for router in routers:
+        net.add_node(router, label=str(router))
 
     # Add edges to graph
-    for edge in graphDictionary:
-        for data in graphDictionary[edge]:
-            net.add_edge(str(edge), str(data), value=2, label=str(graphDictionary[edge][data][0]) + ' ' + graphDictionary[edge][data][1])
+    for branch in branches:
+        for router in range(0, len(branch) - 1):
+            net.add_edge(branch[router], branch[router+1], physics=False)
 
     # Pop browser and show graph
-    net.show('nodes.html')
+    # net.show("nodes.html")
 
 
 def main():
