07230d3106abb21f221104b05f223dcdfb041ffc
Nikhil <nikhil342015@gmail.com>
2023-02-14 01:27:51 -0800

    Main Function Updated

--- main.py
+++ main.py
@@ -31,44 +31,18 @@ def generateVisual(branches, routers):
 
 def main():
     # Initialize graph variables
-    nodes = ["A", "B", "C", "D", "E", "F", "G", "H"]
-    types = ["wireless", "wired"]
-    graphDictionary = {}
-    for node in nodes:
-        graphDictionary[node] = {}
-    
-    # Create graph data
-    graphDictionary["A"]["B"] = [random.randint(1,5), random.choice(list(types))]
-    graphDictionary["A"]["C"] = [random.randint(1,5), random.choice(list(types))]
-    graphDictionary["B"]["D"] = [random.randint(1,5), random.choice(list(types))]
-    graphDictionary["C"]["D"] = [random.randint(1,5), random.choice(list(types))]
-    graphDictionary["C"]["G"] = [random.randint(1,5), random.choice(list(types))]
-    graphDictionary["D"]["G"] = [random.randint(1,5), random.choice(list(types))]
-    graphDictionary["D"]["E"] = [random.randint(1,5), random.choice(list(types))]
-    graphDictionary["D"]["F"] = [random.randint(1,5), random.choice(list(types))]
-    graphDictionary["F"]["H"] = [random.randint(1,5), random.choice(list(types))]
+    routers = []
+    for i in range(1, 21):
+        routers.append(i)
+    print(routers)
 
-    # Make graph object
-    graph = Graph(graphDictionary, nodes)
+    # Create branches
+    branches = [[19, 1, 2, 3, 4, 5],
+                [6, 7, 8, 12, 9, 5],
+                [13, 10, 17, 11, 12, 9, 5],
+                [14, 15, 16, 18, 20, 5]]
 
-    # Generate graph using pyvis
-    generateVisual(graphDictionary, nodes)
-
-    # Perform Dijkstras
-    previousNodes1, shortestPathValues1 = dijkstra(graph=graph, firstNode="A")
-    previousNodes2, shortestPathValues2 = dijkstraMod(graph=graph, firstNode="A")
-
-    targetNode = random.choice(list(nodes))
-    
-    # Display the dijksta path from A to target node
-    try:
-        display(previousNodes1, graphDictionary, firstNode="A", lastNode=targetNode)
-    except:
-        print("Node Failure has occured for the dijkstra algorithm")
-    try:
-        display(previousNodes2, graphDictionary, firstNode="A", lastNode=targetNode)
-    except:
-        print("Node Failure has occured for the modified dijkstra algorithm")
+    generateVisual(branches, routers)
     
     #Console prints the path from G to A with the edge weights
 if __name__ == "__main__":
