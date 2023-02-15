83f267c797ef50ee6ab611fd9568070f8e9ca2ec
Nikhil <nikhil342015@gmail.com>
2023-02-14 19:37:13 -0800

    Node Sampling Inital Implementation

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pyvis.network import Network
 import random
 
 probability_bank = [0.2, 0.4, 0.5, 0.6, 0.8]
+sending_growth_bank = [20, 30, 40, 80, 100]
  
 class Packet(object):
     #Constructor
@@ -30,6 +31,43 @@ def generateVisual(branches, routers):
     # Pop browser and show graph
     # net.show("nodes.html")
 
+def nodeSamplingSingle(branches, normal_packets, attacker_packets, p, growth_factor):
+    iteration = 0
+    for branch in branches:
+        if (iteration == 1):
+            for router in branch:
+                for packet in attacker_packets:
+                    x = random.randint(0, 10)/10
+                    if x <= p:
+                        packet.node = router
+        else:
+            for router in branch:
+                for packet in normal_packets:
+                    x = (random.randint(0, 10))/10
+                    if x <= p:
+                        packet.node = router
+        iteration += 1
+
+    print(iteration)
+    print("Growth Factor: " + str(growth_factor))
+    print("Normal Packet Amount: " + str(len(normal_packets)))
+    print("Attacker Packet Amount: " + str(len(attacker_packets)))
+    print([packet.node for packet in normal_packets])
+    print([packet.node for packet in attacker_packets])
+
+
+    #path reconstruction at victim v:
+    NodeTable = [] # Intialize Node Table
+    for packet in attacker_packets:
+        z = next((x for x in NodeTable if x[0] == packet.node), None) # lookup packet.node in NodeTable
+        if z != None:
+            z[1] += z[1]
+        else:
+            NodeTable.append([packet.node, 1])
+        # Sort table by count
+        NodeTable.sort(key = lambda x: x[1])
+    print(NodeTable)
+
 
 def main():
     # Initialize graph variables
@@ -46,23 +84,13 @@ def main():
 
     generateVisual(branches, routers)
     p = random.choice(probability_bank)
+    growth_factor = random.choice(sending_growth_bank)
 
-    # marking procedure at router r
-    # for packet in packets:
-    #     x = random(0, 10)/10
-    #     if x <= p:
-    #         packet.node = r
+    normal_packets = [Packet() for i in range(10)]
+    attacker_packets = [Packet() for i in range(10*growth_factor)]
 
-    #path reconstruction at victim v:
-    # NodeTable = [] # Intialize Node Table
-    # for packet in attacker_packets:
-    #     z = any(packet.node in sublist for sublist in NodeTable) # lookup packet.node in NodeTable
-    #     if z != None:
-    #         z[1] += z[1]
-    #     else:
-    #         NodeTable.append([packet.node, 1])
-    #     # Sort table by count
-    #     NodeTable.sort(key = lambda x: x[1])
+    nodeSamplingSingle(branches, normal_packets, attacker_packets, p, growth_factor)
+
 
     
     #Console prints the path from G to A with the edge weights
